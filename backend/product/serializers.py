62d2ce3a9a5c6ac9f200e83cdfb8498bfbe782ff
Faris <faris46820@gmail.com>
2023-08-31 19:05:08 +0530

    URLs, Reverse, & Serializers

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -1,20 +1,35 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from product.models import Product
 
 
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field = 'pk' 
+        )
     class Meta:
         model = Product
         fields = [
-            'pk,'
+            'url',
+            'edit_url',
+            'pk',
             'title',
             'content',
             'price',
             'sale_price',
-            'my_discount'
+            'my_discount',
         ]
+        
+    def get_edit_url(self,obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-edit",kwargs={"pk":obj.pk},request=request)
+        # return f"/api/product/{obj.id}/"
     def get_my_discount(self,obj):
         if not hasattr(obj,'id'):
             return None
--- backend/product/urls.py
+++ backend/product/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     # path('',views.product_alt_view),
     # path('',views.product_alt_view),
-    path('',views.product_list_create_view),
-    path('<int:pk>/',views.product_mixin_view),
-    path('<int:pk>/update/',views.product_update_view),
+    path('',views.product_list_create_view,name='product-list'),
+    path('<int:pk>/',views.product_detail_view,name='product-detail'),
+    path('<int:pk>/update/',views.product_update_view,name='product-edit'),
     path('<int:pk>/delete/',views.product_detail_view),
     
     
